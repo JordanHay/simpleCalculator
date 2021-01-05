75a0ea79dcc7dc50db11a5ed7b73e7b4d5bdcf9a
Jordan Haywood <51302288+JordanHay@users.noreply.github.com>
2021-01-05 09:30:58 -0600

    Add files via upload

---
+++ SimpleCalculatorApp/SimpleCalculator/InputConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace SimpleCalculator
+{
+    public class InputConverter
+    {
+        public double ConvertInputToNumeric(string argTextInput)
+        {
+            double convertedNumber;
+            if (!double.TryParse(argTextInput, out convertedNumber))
+                throw new ArgumentException("Expected a numeric value");
+            return convertedNumber;
+        }
+    }
+}
---
+++ SimpleCalculatorApp/SimpleCalculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                InputConverter inputConverter = new InputConverter();
+
+                CalculatorEngine calculatorEngine = new CalculatorEngine();
+
+                double firstNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                double secondNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                string operation = Console.ReadLine();
+
+                double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
+
+                Console.WriteLine(result);
+            }
+            catch(Exception ex)
+            {
+                // in real world we would want to log this message
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
